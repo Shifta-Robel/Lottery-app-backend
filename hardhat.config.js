e749e9e7354c4b32865b9e32bf97a39c543db9a9
robel <robelbire16@gmail.com>
2022-06-27 13:37:42 +0300

    created project and added required dependancies

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-contract-sizer");
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.8",
+};
