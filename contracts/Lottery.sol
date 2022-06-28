7869a2f6185bed096d8e0ae273c92d51f69887c2
robel <robelbire16@gmail.com>
2022-06-28 12:16:34 +0300

    Implemented VRF logic inside Lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,14 +2,42 @@
 
 pragma solidity ^0.8.8;
 
-error Lottery__InsufficientFund();
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
-contract Lottery {
+error Lottery__InsufficientFund();
+error Lottery_TransferFailed();
+
+contract Lottery is VRFConsumerBaseV2 {
     uint private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
 
-    constructor(uint256 entranceFee) {
+    bytes32 private immutable i_keyHash;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callBackGasLimit;
+
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    address private s_latestWinner;
+    /* Events */
+    event lotteryEntered(address indexed player);
+    event requestedWinner(uint256 indexed requestId);
+    event winnerPicked(address indexed winner);
+
+    constructor(
+        address VRFCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 keyHash,
+        uint64 subscriptionId,
+        uint32 callBackGasLimit
+    ) VRFConsumerBaseV2(VRFCoordinatorV2) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(VRFCoordinatorV2);
         i_entranceFee = entranceFee;
+        i_keyHash = keyHash;
+        i_subscriptionId = subscriptionId;
+        i_callBackGasLimit = callBackGasLimit;
     }
 
     function enterLottery() public payable {
@@ -17,9 +45,41 @@ contract Lottery {
             revert Lottery__InsufficientFund();
         }
         s_players.push(payable(msg.sender));
+        emit lotteryEntered(msg.sender);
+    }
+
+    function requestRandomWinner() external {
+        emit requestedWinner(
+            i_vrfCoordinator.requestRandomWords(
+                i_keyHash,
+                i_subscriptionId,
+                REQUEST_CONFIRMATIONS,
+                i_callBackGasLimit,
+                NUM_WORDS
+            )
+        );
+    }
+
+    function fulfillRandomWords(uint256, uint256[] memory randomWords)
+        internal
+        override
+    {
+        uint256 winnerIndex = randomWords[0] % s_players.length;
+        address payable latestWinner = s_players[winnerIndex];
+        s_latestWinner = latestWinner;
+
+        (bool success, ) = latestWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Lottery_TransferFailed();
+        }
+        emit winnerPicked(latestWinner);
     }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function getLatestWinner() public view returns (address) {
+        return s_latestWinner;
     }
 }
