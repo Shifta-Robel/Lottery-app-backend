5d1fcca5a295628c49a8dccef0722e27b0536380
robel <robelbire16@gmail.com>
2022-07-04 14:21:18 +0300

    added subscriptionId for rinkeby

--- helper-hardhat.config.js
+++ helper-hardhat.config.js
@@ -7,7 +7,7 @@ const networkConfig = {
     entranceFee: ethers.utils.parseEther("0.01"),
     gasLane:
       "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-    subscriptionId: "0",
+    subscriptionId: "7797",
     callbackGasLimit: "500000",
     interval: "30",
   },
@@ -22,8 +22,10 @@ const networkConfig = {
 };
 
 const developmentChains = ["hardhat", "localhost"];
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
 
 module.exports = {
   networkConfig,
   developmentChains,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
 };
