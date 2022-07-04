317d61c380e0ad6feb176b3a1c092ba1453cd0dc
robel <robelbire16@gmail.com>
2022-07-04 14:20:34 +0300

    added etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,6 +11,7 @@ require("dotenv").config();
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -41,9 +42,14 @@ module.exports = {
   },
   gasReporter: {
     //enabled: process.env.REPORT_GAS !== undefined,
-    enabled: false,
+    enabled: true,
   },
   mocha: {
     timeout: 200000,
+  },
+  etherscan: {
+    apiKey: {
+      rinkeby: ETHERSCAN_API_KEY,
+    },
   },
 };
